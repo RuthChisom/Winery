09f2c05fdf5b9bce7b6441ec448b13af41526839
Ruth Chisom <ndukwe100805043@gmail.com>
2022-11-29 16:18:17 +0100

    Update drinkhub_app.sql

--- drinkhub_app.sql
+++ drinkhub_app.sql
@@ -5,26 +5,22 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `cat_id` int(11) NOT NULL AUTO_INCREMENT,
-  `cat_title` text NOT NULL,
-  PRIMARY KEY (`cat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 DROP TABLE IF EXISTS `drink`;
 CREATE TABLE `drink` (
   `drk_id` int(11) NOT NULL AUTO_INCREMENT,
-  `drk_category_id` int(11) NOT NULL,
   `drk_name` text NOT NULL,
   `drk_price` float NOT NULL,
-  `drk_description` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`drk_id`),
-  KEY `drk_category_id` (`drk_category_id`),
-  CONSTRAINT `drink_ibfk_1` FOREIGN KEY (`drk_category_id`) REFERENCES `category` (`cat_id`) ON UPDATE CASCADE
+  `drk_image` varchar(100) NOT NULL DEFAULT 'thirddrink.png',
+  PRIMARY KEY (`drk_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `drink` (`drk_id`, `drk_name`, `drk_price`, `drk_image`) VALUES
+(1,	'Moet & Chandon',	30,	'firstdrink.png'),
+(2,	'Belvedere Vodka',	97,	'seconddrink.png'),
+(3,	'Ciroc Blue Vodka',	890,	'thirddrink.png'),
+(4,	'Ciroc Red Dunamis',	180,	'firstdrink.png'),
+(5,	'Velvet Fruit Wine',	30,	'firstdrink.png'),
+(6,	'Chelsea London Gin',	29,	'firstdrink.png');
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -44,4 +40,4 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`usr_id`, `usr_email`, `usr_password`, `usr_firstname`, `usr_lastname`, `usr_phone`, `usr_role`, `usr_registered_date`, `usr_updated_time`, `usr_last_login`) VALUES
 (1,	'jerry@yahoo.com',	'nj123',	'Jerry',	'Ndukwe',	'08100591807',	'admin',	'2022-11-09 16:32:13',	'2022-11-09 16:32:13',	NULL);
 
--- 2022-11-09 16:32:36
+-- 2022-11-29 15:17:49
